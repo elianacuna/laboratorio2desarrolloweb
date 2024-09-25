34e972fa705406c1fb36868df4b397a5d5f45f43
elianacuna <83922885+elianacuna@users.noreply.github.com>
2024-09-24 21:48:37 -0600

    hola

--- modules/productos/agregar_producto.php
+++ modules/productos/agregar_producto.php
@@ -10,7 +10,7 @@ include_once '../../includes/conexion.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ejemplo3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../assets/css/cliente.css">
+    <link rel="stylesheet" href="../../assets/css/producto.css">
   </head>
   <body>
 
